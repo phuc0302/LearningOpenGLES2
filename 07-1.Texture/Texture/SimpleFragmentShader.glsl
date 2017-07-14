10acbec09725f4226613ded7a4f2c4c5c22e0dda
Phuc, Tran Huu <phuc@fiisionstudio.com>
2017-07-15 00:49:10 +0700

    Clearout source.

--- 07-1.Texture/Texture/SimpleFragmentShader.glsl
+++
@@ -1,8 +0,0 @@
-uniform sampler2D u_Texture;
-
-varying lowp vec4 frag_Color;
-varying lowp vec2 frag_TexCoord;
-
-void main(void) {
-    gl_FragColor = frag_Color * texture2D(u_Texture, frag_TexCoord);
-}
